4255ad82836d36d9d942a42a6420adf4a2b121f4
Matthias Harden <matthias@lunacylabs.ca>
2023-07-07 14:28:31 -0700

    add the tree and interactive components

--- data/units/player/player.gd
+++ data/units/player/player.gd
@@ -4,6 +4,9 @@ extends Unit
 
 const MOVE_SPEED: float = 200
 
+var interactables: Array[Area2D] = []
+var currently_interactive: Area2D
+
 @onready var den_scene: PackedScene = preload("res://data/structures/den/den.tscn")
 
 func _physics_process(delta: float) -> void:
@@ -19,11 +22,39 @@ func _physics_process(delta: float) -> void:
 		movement.x -= 1
 	if Input.is_action_just_pressed("perform_ability"):
 		perform_ability()
+	if Input.is_action_just_pressed("interact"):
+		handle_interact()
 	
 	move_and_collide(movement.normalized() * MOVE_SPEED * delta)
+
 
 func perform_ability() -> void:
 	# For now just spawning a den
 	var den: Structure = den_scene.instantiate()
 	den.global_position = Vector2(global_position.x, global_position.y - 50)
 	GlobalGameState.game.add_child(den)
+
+func handle_interact() -> void:
+	if !currently_interactive:
+		return
+	print("Interacted with ", currently_interactive)
+
+
+func _on_interactable_searcher_area_entered(area: Area2D) -> void:
+	interactables.append(area)
+	choose_currently_interactive()
+
+
+func _on_interactable_searcher_area_exited(area: Area2D) -> void:
+	interactables.remove_at(interactables.find(area))
+	choose_currently_interactive()
+
+
+func choose_currently_interactive():
+	var closest_area: Area2D
+	
+	for area in interactables:
+		if !closest_area || global_position.distance_to(area.global_position) < global_position.distance_to(closest_area.global_position):
+			closest_area = area
+	
+	currently_interactive = closest_area
