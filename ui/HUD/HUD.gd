2abe512dd2b50a045d2340155132f27fa8014907
Matthias Harden <matthias@lunacylabs.ca>
2023-07-07 16:55:41 -0700

    add some stuff

--- ui/HUD/HUD.gd
+++ ui/HUD/HUD.gd
@@ -2,6 +2,7 @@ class_name HUD
 extends CanvasLayer
 
 var currency = 30
+var current_ability = 0
 
 func _ready() -> void:
 	GlobalGameState.HUD = self
@@ -9,3 +10,12 @@ func _ready() -> void:
 func update_currency(amount):
 	currency += amount
 	$CurrencyContainer/CurrencyLeft.text = str(currency)
+
+func cycle_ability(change) -> void:
+	current_ability += change
+	if current_ability < 0:
+		current_ability = 0
+	if current_ability > $AbilityContainer.get_child_count() -1:
+		current_ability = $AbilityContainer.get_child_count() -1
+	
+	$AbilityContainer.get_children()[current_ability]
