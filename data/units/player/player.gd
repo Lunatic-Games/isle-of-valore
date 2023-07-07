4b763f849e6e150255e0b7f30948bb872afa5557
Matthias Harden <matthias@lunacylabs.ca>
2023-07-07 13:40:37 -0700

    add spawning a den

--- data/units/player/player.gd
+++ data/units/player/player.gd
@@ -4,6 +4,7 @@ extends Unit
 
 const MOVE_SPEED: float = 200
 
+@onready var den_scene: PackedScene = preload("res://data/structures/den/den.tscn")
 
 func _physics_process(delta: float) -> void:
 	var movement := Vector2()
@@ -16,5 +17,13 @@ func _physics_process(delta: float) -> void:
 		movement.y += 1
 	if Input.is_action_pressed("move_left"):
 		movement.x -= 1
+	if Input.is_action_just_pressed("perform_ability"):
+		perform_ability()
 	
 	move_and_collide(movement.normalized() * MOVE_SPEED * delta)
+
+func perform_ability() -> void:
+	# For now just spawning a den
+	var den: Structure = den_scene.instantiate()
+	den.global_position = Vector2(global_position.x, global_position.y - 50)
+	GlobalGameState.game.add_child(den)
