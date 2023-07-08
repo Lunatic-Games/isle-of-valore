722496b8cc8f26dd06fcb55f156ef3b322508a70
Matthias Harden <matthias@lunacylabs.ca>
2023-07-07 20:08:07 -0700

    update wolf den and ability HUD

--- ui/HUD/AbilitySlotUI.gd
+++ ui/HUD/AbilitySlotUI.gd
@@ -1,1 +1,10 @@
 extends ColorRect
+
+@export var sprite: Texture2D
+@export var cost: int
+@onready var currency_sprite = $TextureRect
+@onready var currenct_text = $CurrencyContainer/CurrencyLeft
+
+func _ready() -> void:
+	currency_sprite.texture = sprite
+	currenct_text.text = str(cost)
