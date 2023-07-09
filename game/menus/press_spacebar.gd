fcd0b7cb455c18efabbcfaa1c9a618419d6eedc8
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-07-09 09:25:29 -0700

    Intro cutscene

--- game/menus/press_spacebar.gd
+++ game/menus/press_spacebar.gd
@@ -1,6 +1,11 @@
 extends ColorRect
 
 
+@onready var human_1: Human = $"../../Human"
+@onready var human_2: Human = $"../../Human2"
+@onready var human_3: Human = $"../../Human3"
+@onready var human_4: Human = $"../../Human4"
+
 func _ready():
 	if visible:
 		get_tree().paused = true
@@ -14,6 +19,24 @@ func _input(event):
 		$AnimationPlayer.play("intro")
 
 
-func unpause():
+func play_dialogue_1():
+	human_1.dialogue.display_dialogue("Y'all ready to play?", true)
+
+func play_dialogue_2():
+	human_2.dialogue.display_dialogue("Heck yeah, let's start cutting down some trees for wood.", true)
+
+func play_dialogue_3():
+	human_3.dialogue.display_dialogue("There should be animals around we can hunt for food.", true)
+
+func play_dialogue_4():
+	human_4.dialogue.display_dialogue("Hopefully we can reach the end-game before they destroy our base!", true)
+	
+func play_dialogue_5():
+	human_1.dialogue.display_dialogue("I'll show those animals no mercy...", true)
+
+func play_dialogue_6():
+	human_2.dialogue.display_dialogue("That last one sounded intentionally evil for the sake of plot...", true)
+
+func done_cutscene():
 	get_tree().paused = false
 	GlobalGameState.game_started.emit()
