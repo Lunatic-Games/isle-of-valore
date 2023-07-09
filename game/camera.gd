576f45d9bbc11b0f9ad7ebc3f7e34d33df767cd0
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-07-09 07:23:41 -0700

    Started working on menus

--- game/camera.gd
+++ game/camera.gd
@@ -2,8 +2,12 @@ extends Camera2D
 
 
 @export var follow_target: Node2D
+@export var follow_enabled: bool = false
 
 
 func _physics_process(_delta: float) -> void:
+	if follow_enabled == false:
+		return
+	
 	if follow_target:
 		global_position = follow_target.global_position
--- global/game_state.gd
+++ global/game_state.gd
@@ -1,5 +1,8 @@
 extends Node
 
+
+signal game_started
+
 var game: Game = null
 var HUD: HUD = null
 var infuse_controller = null
